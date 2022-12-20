665f581eb1ef53c9088e46b408c66c9f2db90711
veeresh-27 <veereshrm789@gmail.com>
2022-12-20 19:42:35 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bengaluru, india - 560036 </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span> Veeresh | Vijay</span> </p>
+
+</section>
